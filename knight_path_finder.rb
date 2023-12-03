9ac350e869574ae65fa0a9cf2f7943e484fb4414
Wojciech <klonowski.wojtek@yahoo.co.uk>
2023-12-03 10:59:51 +0100

    slowly building KPF class

--- knight_path_finder.rb
+++ knight_path_finder.rb
@@ -2,9 +2,18 @@ require_relative 'polytree_node'
 
 class KnightPathFinder
 
+    attr_reader :position, :previous_position, :jumps
 
-    def initialize(position)
-        @position = position
+    def initialize(starting_position)
+        @position = starting_position
+        @previous_position = nil
+        @jumps = []
+    end
+
+    def previous_position=(node)
+        @previous_position.jumps.delete(self) unless @previous_position.nil?
+        @previous_position = node
+        @previous_position.children << self unless @previous_position.nil?
     end
     
 end
