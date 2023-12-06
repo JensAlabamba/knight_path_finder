f4a9ee3856e8fb4f7af9f394bd386536d7b31a2e
Wojciech <klonowski.wojtek@yahoo.co.uk>
2023-12-06 09:31:26 +0100

    move validation

--- knight_path_finder.rb
+++ knight_path_finder.rb
@@ -4,6 +4,32 @@ class KnightPathFinder
 
     attr_reader :position, :previous_position, :jumps
 
+    MOVES = {
+        [-2, -1]
+        [-2, 1]
+        [-1, -2]
+        [-1, 2]
+        [1, -2]
+        [1, 2]
+        [2, -1]
+        [2, 1]
+    }
+
+    def self.valid_moves(current_pos)
+        valid_moves = []
+        cur_x, cur_y = current_pos
+        
+        MOVES.each do |(x, y)|
+            new_position = [cur_x + x, cur_y + y]
+
+            if new_position.all? { |coord| coord.between?(0, 7) }
+
+            valid_moves << new_position
+            end
+        end
+        valid_moves
+    end
+
     def initialize(starting_position)
         @position = starting_position
         @previous_position = nil
